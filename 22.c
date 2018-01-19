e5eed9ee61f084a9b89c70f3b7b0eda4206950b9
gayathrimogan <31534707+gayathrimogan@users.noreply.github.com>
2018-01-19 15:10:44 +0530

    Update 22.c

--- 22.c
+++ 22.c
@@ -1,25 +1,25 @@
 include <stdio.h>
  
 
-int gcd(int s, int t)
+int gcd(int n, int t)
 {
 
-    if (s == 0 || t == 0)
+    if (n== 0 || t == 0)
        return 0;
  
 
-    if (s== t)
+    if (n== t)
         return a;
  
-    if (s > t)
-        return gcd(s-t, s);
-    return gcd(s, t-s);
+    if (n> t)
+        return gcd(n-t, n);
+    return gcd(n, t-n);
 }
  
 
 int main()
 {
-    int s = 78, t = 54;
-    printf("GCD of %d and %d is %d ", s, t, gcd(s, t));
+    int n = 78, t = 54;
+    printf("GCD of %d and %d is %d ", n, t, gcd(n, t));
     return 0;
 }
