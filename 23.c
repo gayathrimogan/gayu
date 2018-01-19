5458078a7c81a5786d4c48d5f424c402c2a0f1db
gayathrimogan <31534707+gayathrimogan@users.noreply.github.com>
2018-01-19 15:14:43 +0530

    Update 23.c

--- 23.c
+++ 23.c
@@ -2,19 +2,19 @@
 int hcf(int x,int y); 
 void main() 
 { 
-    int i,j,k; 
+    int i,j,e; 
     clrscr(); 
-    printf("Enter 2 Numbers : "); 
+    printf("Enter 2 Num : "); 
     scanf("%d%d",&i,&j); 
     if(i>j) 
       { 
-           k=hcf(i,j); 
+           e=hcf(i,j); 
       } 
     else 
      { 
-           k= hcf(i,j); 
+           e= hcf(i,j); 
      } 
-           printf("HCF is= %d",k); 
+           printf("HCF is= %d",e); 
            getch(); 
 } 
     int hcf(int a,int b) 
