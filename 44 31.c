7757cf866c6e2c881995143e217471b7d0d20206
gayathrimogan <31534707+gayathrimogan@users.noreply.github.com>
2018-01-31 16:36:16 +0530

    Create 44 31.c

---
+++ 44 31.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+int n;
+printf("enter the number");
+scanf("%d",&n);
+if(n<10)
+{
+printf("yes it is with in range %d",n);
+}
+else
+{
+printf("it is not in range %d",n);
+}
+}
