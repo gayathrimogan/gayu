472c57b1777c96f3cdd45ca7d872b7fadf6d567e
gayathrimogan <31534707+gayathrimogan@users.noreply.github.com>
2018-02-02 15:16:16 +0530

    Create 53.c

---
+++ 53.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+int a;
+sum=0;
+printf("enter the value");
+scanf("%d",&a);
+    while(a!=0)
+    {
+        m=a%10;
+        a=a/10;
+        sum=sum+m;
+    }
+    printf("%d",sum);
+    }{
+i=n%10;
