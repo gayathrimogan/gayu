6afa6f7a6347a9f8254d9e8dc400c67559ac6f12
gayathrimogan <31534707+gayathrimogan@users.noreply.github.com>
2018-02-02 14:31:41 +0530

    Create 49.c

---
+++ 49.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+int a,b,c;
+printf("enter the a value");
+scanf("%d",a);
+printf("enter the b value");
+scanf("%d",b);
+c=a+b;
+printf("the sum of two number is %d",c);
+getch();
+}
