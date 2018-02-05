aa73e8a7d75ec4fbe39c2bd9393d1eb162ab3076
gayathrimogan <31534707+gayathrimogan@users.noreply.github.com>
2018-02-05 16:07:57 +0530

    Create 54.c

---
+++ 54.c
@@ -0,0 +1,19 @@
+
+#include<stdio.h>
+void main()
+{
+    int n,l;
+
+printf("enter the nnumber");
+scanf("%d",&n);
+if(n%2==0)
+{
+   printf("%d",n);
+}
+else
+{
+
+ l=n-1;
+printf("the number  is %d",l);
+}
+}
