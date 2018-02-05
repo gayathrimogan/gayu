b572c844e3b5e0152834ecc34c3bd86ad0601aee
gayathrimogan <31534707+gayathrimogan@users.noreply.github.com>
2018-02-05 16:14:32 +0530

    Create 55.c

---
+++ 55.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+{
+    int m,n,c;
+
+printf("enter the nnumber");
+scanf("%d",&m);
+printf("enter the nnumber");
+scanf("%d",&n);
+ c=m*n;
+ if(c/2)
+ {
+     printf("it is even number");
+ }
+else
+{
+    printf("it is odd number");
+}
+}
