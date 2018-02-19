8c3eb766a78423ba73749f478607a584fc1711b2
gayathrimogan <31534707+gayathrimogan@users.noreply.github.com>
2018-02-19 16:15:22 +0530

    Update 60 19.c

--- 60 19.c
+++ 60 19.c
@@ -1,15 +1,15 @@
 #include<stdio.h>
-voidmain()
+void main()
 {
 int n,i,a=0,b=1;
 printf("enter the number");
 scanf("%d",&n);
-for(i=0;i<n;i++)
+for(i=1;i<=n;i++)
 {
 printf("%d",i);
 c=a+b;
 a=b;
 b=c;
-printf("the result is %d",b);
 }
+printf("the result is %d",a);
 }
