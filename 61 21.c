a3ef4bbdb78b57dd9fc21936d60bb5ad15e66005
gayathrimogan <31534707+gayathrimogan@users.noreply.github.com>
2018-02-21 16:13:02 +0530

    Rename 61.c to 61 21.c

---
+++ 61 21.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+char a[10];
+int k,i;
+printf("enter the string");
+scanf("%s",&a[i]);
+printf("enter the number");
+scanf("%d",&k);
+for(i=o;i<k;i++)
+{
+printf("%c",a[i]);
+}
+}
